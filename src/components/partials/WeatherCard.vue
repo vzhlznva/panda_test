24cb2cd89d6fcd6e836e125379409bdb01e0df97
Valeriia Zhelieznova <lzheleznova2609@gmail.com>
2024-10-28 05:42:27 +0200

    fix dynamic images processing

--- src/components/partials/WeatherCard.vue
+++ src/components/partials/WeatherCard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { ReducedWeatherChartItem, WeatherChartItem } from '/@src/types/weather';
 import { formatTime } from '/@src/utils/formatters';
 
@@ -10,12 +10,27 @@ const props = defineProps<
   }
 >()
 
+const weatherImages = ref()
+const weatherImage = ref<string | undefined>(undefined)
+
+const getImageIcon = async (icon: string): Promise<string | undefined> => {
+  weatherImages.value = import.meta.glob('/src/assets/images/weather/*.png')
+  if (weatherImages.value[`/src/assets/images/weather/${icon}.png`]) {
+    const module = await weatherImages.value[`/src/assets/images/weather/${icon}.png`]()
+    return module.default
+  } else return undefined
+}
+
+onMounted(async () => {
+  weatherImage.value = await getImageIcon(props.item.weather[0].icon) as string
+  console.log(weatherImage.value, weatherImages.value)
+})
 </script>
 
 <template>
   <div class="weather-card">
     <p>{{ days === 1 ? formatTime(item.day) : item.day }}</p>
-    <img :src="`/~/images/weather/${item.weather[0]?.icon}.png`">
+    <img :src="weatherImage">
     <p class="day-temp">{{ Math.round(item.max) }}&deg;C</p>
     <p class="night-temp" v-if="days == 5">{{ Math.round(item.min) }}&deg;C</p>
   </div>
